2b4a5f66db9860b96aefb3898da975191b7799a3
JacobG58014 <bocaj.griffiths@gmail.com>
2015-11-20 16:07:20 +0000

    20/11/2015
    Working Camera

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/64/50d5e58d9f8f001512cbc3386653b96f
@@ -0,0 +1,28 @@
+package Application;
+
+public class Camera {
+
+	private float x, y;
+	
+	public Camera(float x, float y){
+		this.x = x;
+		this.y = y;
+	}
+	public float getX(){
+		return x;
+	}
+	public float getY(){
+		return y;
+	}
+	
+	public void setX(){
+		this.x = x;
+	}
+	
+	public void setY(){
+		this.y = y;
+	}
+	
+	
+
+}
--- RPG/src/Application/Camera.java
+++ RPG/src/Application/Camera.java
@@ -1,28 +1,67 @@
 package Application;
 
+import java.awt.event.KeyEvent;
+
 public class Camera {
 
-	private float x, y;
+	
+	double camX = 0, camY = 0, x, y;
 	
 	public Camera(float x, float y){
-		this.x = x;
-		this.y = y;
+		this.camX = camX;
+		this.camY = camY;
+
 	}
-	public float getX(){
-		return x;
+	public double getCamX(){
+		return camX;
 	}
-	public float getY(){
-		return y;
-	}
-	
-	public void setX(){
-		this.x = x;
+	public double getCamY(){
+		return camY;
 	}
 	
-	public void setY(){
-		this.y = y;
+	public void setCamX(){
+		this.camX = camX;
 	}
 	
+	public void setCamY(){
+		this.camY = camY;
+	}
 	
+public void right(){
+	 camX = 5;
+}
+public void left(){
+	 camX = -5;
 
 }
+public void up(){
+	 camY = 5;
+
+}
+public void down(){
+	 camY = -5;
+
+}
+public void righto(){
+	 camX = 0;
+}
+public void lefto(){
+	 camX = 0;
+
+}
+public void upo(){
+	 camY = 0;
+
+}
+public void downo(){
+	 camY = 0;
+
+}
+public void tick() {
+	  x+=camX;
+
+	  y+=camY;	
+}
+	}
+	
+
